89fda5d9844f335dab11635ee6386019ef078f72
gvkholm <glebkholmanskikh@yandex.ru>
2026-06-20 02:08:17 +0300

    third_task
    Implemented a pipe to connect two processes executing CMD1 and CMD2, redirecting output from CMD1 to CMD2.

--- problem3-pipe-connect-2-processes/solution.c
+++ problem3-pipe-connect-2-processes/solution.c
@@ -3,22 +3,47 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-// Программе передаётся два аргумента: CMD1 и CMD2.
-// Необходимо запустить два процесса, выполняющих эти команды,
-// и перенаправить стандартный поток вывода CMD1 на стандартный поток ввода CMD2.
-// Эквивалентно: CMD1 | CMD2
-// Родительский процесс должен завершаться самым последним.
-
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <CMD1> <CMD2>\n", argv[0]);
         return 1;
     }
-
-    // TODO: создайте канал (pipe),
-    //       запустите CMD1 (argv[1]) так, чтобы его stdout → write-конец канала,
-    //       запустите CMD2 (argv[2]) так, чтобы его stdin  ← read-конец канала,
-    //       дождитесь завершения обоих дочерних процессов.
-
+    const char *cmd1 = argv[1];
+    const char *cmd2 = argv[2];
+    int fds[2];
+    if (pipe(fds) < 0) {
+        perror("pipe");
+        return 1;
+    }
+    pid_t pid1 = fork();
+    if (pid1 < 0) {
+        perror("fork");
+        return 1;
+    }
+    if (pid1 == 0) {
+        close(fds[0]);
+        dup2(fds[1], STDOUT_FILENO);
+        close(fds[1]);
+        execlp("sh", "sh", "-c", cmd1, (char *)NULL);
+        perror("execlp");
+        _exit(127);
+    }
+    pid_t pid2 = fork();
+    if (pid2 < 0) {
+        perror("fork");
+        return 1;
+    }
+    if (pid2 == 0) {
+        close(fds[1]);
+        dup2(fds[0], STDIN_FILENO);
+        close(fds[0]);
+        execlp("sh", "sh", "-c", cmd2, (char *)NULL);
+        perror("execlp");
+        _exit(127);
+    }
+    close(fds[0]);
+    close(fds[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
     return 0;
 }
