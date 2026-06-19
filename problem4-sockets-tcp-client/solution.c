2c9b8125f8513911cae9caca559892585d55e376
gvkholm <glebkholmanskikh@yandex.ru>
2026-06-20 02:09:02 +0300

    fourth_task
    Implement TCP client to connect to server and send/receive integers.

--- problem4-sockets-tcp-client/solution.c
+++ problem4-sockets-tcp-client/solution.c
@@ -6,28 +6,42 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-// Программе передаются два аргумента:
-//   argv[1] — IPv4-адрес сервера в десятичной записи (например, "127.0.0.1")
-//   argv[2] — номер порта
-//
-// Программа должна:
-//   1. Установить TCP-соединение с указанным сервером.
-//   2. В цикле читать со stdin целые знаковые числа в текстовом формате.
-//   3. Отправлять каждое число на сервер в бинарном виде (int32, Little Endian).
-//   4. Получать от сервера int32 LE в ответ и выводить его в stdout в текстовом виде.
-//   5. Если сервер закрыл соединение — завершиться с кодом возврата 0.
-
-int main(int argc, char *argv[]) {
+int main (int argc, char **argv) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <ipv4_addr> <port>\n", argv[0]);
         return 1;
     }
-
-    // TODO: создайте TCP-сокет (AF_INET, SOCK_STREAM),
-    //       заполните struct sockaddr_in с помощью inet_aton/inet_pton,
-    //       подключитесь через connect,
-    //       реализуйте цикл чтения/отправки/приёма/вывода чисел.
-    //       Порядок байт — Little Endian (на x86/x86_64 это нативный порядок).
-
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(atoi(argv[2])),
+        .sin_addr.s_addr = inet_addr(argv[1]),
+    };
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        close(sock);
+        return 1;
+    }
+    int n;
+    while (scanf("%d", &n) == 1) {
+        if (write(sock, &n, sizeof(n)) < 0) {
+            perror("write");
+            close(sock);
+            return 1;
+        }
+        int res;
+        ssize_t r = read(sock, &res, sizeof(res));
+        if (r < 0) {
+            close(sock);
+            return 1;
+        }
+        printf("%d\n", res);
+        fflush(stdout);
+    }
+    close(sock);
     return 0;
 }
